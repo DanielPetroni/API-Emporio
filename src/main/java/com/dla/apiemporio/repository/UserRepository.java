b584f6513c5bf8aa477df6283fe5f27916b02d81
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-13 16:07:47 -0300

    created repository User

---
+++ src/main/java/com/dla/apiemporio/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dla.apiemporio.repository;
+
+import com.dla.apiemporio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

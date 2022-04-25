86f3db30f383e5ea08c44be8812e5e793f768782
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-24 21:19:08 -0300

    created model cliente

---
+++ src/main/java/com/dla/apiemporio/model/Cliente.java
@@ -0,0 +1,74 @@
+package com.dla.apiemporio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+    public Cliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    @Id
+    @GeneratedValue
+    private Long idCliente;
+
+    private String nomeCliente;
+
+    @Column(unique = true)
+    private String cpfCliente;
+
+    private String emailCliente;
+
+    private String telefoneCliente;
+
+    @ManyToOne
+    @JoinColumn(name = "idEndereco")
+    private Endereco idEndereco;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public String getEmailCliente() {
+        return emailCliente;
+    }
+
+    public void setEmailCliente(String emailCliente) {
+        this.emailCliente = emailCliente;
+    }
+
+    public String getTelefoneCliente() {
+        return telefoneCliente;
+    }
+
+    public void setTelefoneCliente(String telefoneCliente) {
+        this.telefoneCliente = telefoneCliente;
+    }
+
+    public Endereco getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(Endereco idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+}

3ae2954be583e91390a9bfb41fd0ef2bb6d4531b
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-24 21:34:53 -0300

    Ajusted repository cliente

--- src/main/java/com/dla/apiemporio/repository/ClienteRepository.java
+++ src/main/java/com/dla/apiemporio/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.dla.apiemporio.repository;
 
+import java.util.List;
+
 import com.dla.apiemporio.model.Cliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
+    List<Cliente> findBycpfCliente(Long cpfCliente);
 }

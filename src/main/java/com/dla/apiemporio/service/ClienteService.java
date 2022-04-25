fc81de900b7f4a85b55b3cc2e31686fd9ce018c7
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-24 21:35:41 -0300

    created service cliente

---
+++ src/main/java/com/dla/apiemporio/service/ClienteService.java
@@ -0,0 +1,51 @@
+package com.dla.apiemporio.service;
+
+import java.util.List;
+
+import com.dla.apiemporio.model.Cliente;
+import com.dla.apiemporio.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ClienteService {
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        return clienteRepository.getById(id);
+    }
+
+    public Cliente save(Cliente cliente) throws Exception {
+        List<Cliente> clientes = clienteRepository.findBycpfCliente(cliente.getCpfCliente());
+        if (clientes.size() > 0) {
+            throw new Exception("Cliente já cadastrado!");
+        }
+        clienteRepository.save(cliente);
+        return cliente;
+    }
+
+    public void update(Long id, Cliente cliente) throws Exception {
+        Cliente clienteFinded = clienteRepository.getById(id);
+        if (clienteFinded != null) {
+            clienteFinded = cliente;
+            clienteRepository.save(clienteFinded);
+        } else {
+            throw new Exception("Produto não encontrado!");
+        }
+
+    }
+
+    public void delete(Long id) throws Exception {
+        Cliente clienteFinded = clienteRepository.getById(id);
+        if (clienteFinded != null) {
+            clienteRepository.deleteById(id);
+        } else {
+            throw new Exception("Cliente não encontrado!");
+        }
+
+    }
+}

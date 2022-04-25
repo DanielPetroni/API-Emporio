610f762a326bfe3dd467043850dbd2fa6ec859db
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-24 21:07:29 -0300

    created repository endereco

---
+++ src/main/java/com/dla/apiemporio/repository/EnderecoRespository.java
@@ -0,0 +1,9 @@
+package com.dla.apiemporio.repository;
+
+import com.dla.apiemporio.model.Endereco;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRespository extends JpaRepository<Endereco, Long> {
+
+}
